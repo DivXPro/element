8645777b946b96204e8b3b2c49e4c3b823cfcd61
Huhui <wishgood@gmail.com>
2018-09-07 14:16:51 +0800

    fix

--- build/config.js
+++ build/config.js
@@ -10,26 +10,32 @@ var transitionList = fs.readdirSync(path.resolve(__dirname, '../src/transitions'
 var externals = {};
 
 Object.keys(Components).forEach(function(key) {
-  externals[`element-ui/packages/${key}`] = `element-ui/lib/${key}`;
+  externals[`element-ui/packages/${key}`] = `element-ui-divxpro/lib/${key}`;
 });
 
-externals['element-ui/src/locale'] = 'element-ui/lib/locale';
+externals['element-ui/src/locale'] = 'element-ui-divxpro/lib/locale';
 utilsList.forEach(function(file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/utils/${file}`] = `element-ui/lib/utils/${file}`;
+  externals[`element-ui/src/utils/${file}`] = `element-ui-divxpro/lib/utils/${file}`;
 });
 mixinsList.forEach(function(file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/mixins/${file}`] = `element-ui/lib/mixins/${file}`;
+  externals[`element-ui/src/mixins/${file}`] = `element-ui-divxpro/lib/mixins/${file}`;
 });
 transitionList.forEach(function(file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/transitions/${file}`] = `element-ui/lib/transitions/${file}`;
+  externals[`element-ui/src/transitions/${file}`] = `element-ui-divxpro/lib/transitions/${file}`;
 });
 
-externals = [Object.assign({
-  vue: 'vue'
-}, externals), nodeExternals()];
+externals = [
+  Object.assign(
+    {
+      vue: 'vue'
+    },
+    externals
+  ),
+  nodeExternals()
+];
 
 exports.externals = externals;
 
@@ -53,7 +59,5 @@ exports.postcss = function(webapck) {
   saladConfig.features.partialImport = {
     addDependencyTo: webapck
   };
-  return [
-    require('postcss-salad')(saladConfig)
-  ];
+  return [require('postcss-salad')(saladConfig)];
 };
